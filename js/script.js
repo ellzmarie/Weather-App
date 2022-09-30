d2f4c51ce23e196c969c024ab371275fb093f5d4
ellzmarie <elliematutisdigal@gmail.com>
2022-09-29 20:56:23 -0700

    update api key

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ submitBtn.addEventListener("click", (e) => {
     e.preventDefault()
     console.log(inputBox.value)
   fetch(`https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid=${inputBox.value}`)
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputBox.value}&limit={limit}&appid={a900b9c844bb20ffaaf85e81c6058302}`)
     .then((id) => id.json())
     .then((data) => {
         console.log(data)
@@ -17,4 +18,3 @@ submitBtn.addEventListener("click", (e) => {
     })
     .catch((err) => console.log(err));
 });
-
