15de7c75888c023765b22e0801e66f764b41bd92
ellzmarie <elliematutisdigal@gmail.com>
2022-09-30 10:38:17 -0700

    update fetch

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 const submitBtn = document.querySelector("button");
 const inputBox = document.querySelector(".input");
-const weatherFor = document.querySelector("#weatherFor")
+const weatherForCity = document.querySelector("#weatherFor")
 const getTemp = document.querySelector("#temp") 
 const feelsLike = document.querySelector("#feels")
 const weatherDesc = document.querySelector("#weatherDesc")
@@ -9,11 +9,19 @@ const weatherDesc = document.querySelector("#weatherDesc")
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault()
     console.log(inputBox.value)
-  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputBox.value}&limit={limit}&appid={a900b9c844bb20ffaaf85e81c6058302}`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code}&limit={limit}&appid={a900b9c844bb20ffaaf85e81c6058302}`)
+//   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputBox.value}&limit={limit}&appid={a900b9c844bb20ffaaf85e81c6058302}`)
     .then((id) => id.json())
     .then((data) => {
         console.log(data)
+        weatherForCity.innerText = data.inputBox
+        getTemp.innerText = daily.temp
         feelsLike.innerText = daily.feels_like
+        weatherDesc.innerText = daily.weather
     })
     .catch((err) => console.log(err));
 });
+
+// change from celsius to farenheit
+// http://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&units={units}
+//units=imperial
